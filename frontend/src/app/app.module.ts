bf81dbc11c292cbd1f880f20c67404053eaf734b
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-08-25 10:17:03 +0200

    add rooting

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { ButtonComponent } from './button/button.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    ButtonComponent
+    ButtonComponent,
+    LoginPageComponent,
+    PageNotFoundComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
