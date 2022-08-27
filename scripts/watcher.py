280bc525ed5a90a8593b92e62cd92461c36b2ec1
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-08-27 10:50:40 +0200

    feat docker support - improve watcher

--- scripts/watcher.py
+++ scripts/watcher.py
@@ -1,12 +1,13 @@
 from github_webhook import Webhook
-from flask import Flask
-import sys
+from flask import Flask, request, json
 import subprocess
 import os
-from git import Repo, Remote
+from git import Repo
 
 #print('This is error output', file=sys.stderr)
 #print('This is standard output', file=sys.stdout)
+
+DEPLOYMENT_BRANCH = "feat_front_Docker-support"
 
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
@@ -15,26 +16,39 @@ webhook = Webhook(app) # Defines '/postreceive' endpoint
 def root_endpoint():
     return "Nothing here."
 
-global sub
 @app.route("/",methods = ['POST'])
 def on_push():
-    currdir = os.getcwd()
-    currdir = currdir.split("/")
-    if currdir[-1] == 'OnlyOne':
+    ret = "No change detected"
+    branch = json.loads(request.data)["ref"].split("/")[-1]
+    if branch != DEPLOYMENT_BRANCH:
+        return ret
+    currdir = os.getcwd().split("/")[-1]
+    #dockerps = subprocess.getoutput("docker compose ps --format json")
+    #dockerps = json.loads(dockerps)
+    #print(check_running(dockerps, "onlyoneFrontend"))
+    if currdir == 'OnlyOne':
         repo = Repo()
+        repo.git.checkout(DEPLOYMENT_BRANCH)
         currentCommit = repo.head.commit
         repo.remotes.origin.pull()
         if currentCommit != repo.head.commit:
             subprocess.run(["docker-compose","stop"])
             subprocess.run(["docker-compose","build"])
             subprocess.run(["docker-compose","up", "-d"])
-        else:
-            print("No change detected")
+            ret = "Docker reloaded"
     else:
         Repo.clone_from("https://github.com/OneSock-inc/OnlyOne.git", "./OnlyOne")
         os.chdir("./OnlyOne")
         subprocess.run(["docker-compose","up", "-d"])
-    return "tg post"
+        ret = "Docker started."
+    return ret
+
+
+def check_running(dockerpsOutput, serviceName):
+    for service in dockerpsOutput:
+        if service["Name"] == serviceName:
+            return service["State"] == "running"
+    return False
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9999, debug=True)
