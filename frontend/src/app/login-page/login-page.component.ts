b36dcfafc1ca2a2fdebb263c28f15c9b7ea5f451
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-08-29 16:40:41 +0200

    make login page with basic UI (#23)

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
+  host: {'class': 'default-layout'}
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  title!: string;
+  username!: string;
+  password!: string;
+
+
 
   ngOnInit(): void {
+    this.title = 'Login';
+  }
+
+  onSubmit(form: any): void {
+    console.log(form);
+    alert("Connection successful");
+    // send to api
+    //form.username
+    //form.password
   }
 
 }
