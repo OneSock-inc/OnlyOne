73e4885e8f044516a5e05b03bcf0bac5b51461dc
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-09-01 10:27:44 +0200

    fix test configuration

--- frontend/src/app/services/authService/access-control.service.spec.ts
+++ frontend/src/app/services/authService/access-control.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+
 import { AccessControlService } from './access-control.service';
+import { services } from '..';
 
 describe('AccessControlService', () => {
   let service: AccessControlService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, HttpClientModule],
+      providers: [services, HttpClient],
+    });
     service = TestBed.inject(AccessControlService);
   });
 
