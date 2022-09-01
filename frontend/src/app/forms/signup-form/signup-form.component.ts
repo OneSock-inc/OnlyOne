542eb1b84d663237e9aa68c62dcc66ab90aafcec
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-09-01 21:31:49 +0200

    move signup form into separate component

--- frontend/src/app/forms/signup-form/signup-form.component.ts
+++ frontend/src/app/forms/signup-form/signup-form.component.ts
@@ -1,15 +1,109 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/dataModel/user.model';
+import { countryValidator, postalCodeValidator } from './../customValidators';
+import jsonFile from './countries.json';
+import { MatAutocomplete } from '@angular/material/autocomplete';
+import { Observable } from 'rxjs/internal/Observable';
+import {map, startWith} from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.scss']
+  styleUrls: ['./signup-form.component.scss'],
 })
 export class SignupFormComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
+  private newUser!: User;
+  
+  // Accessed in template
+  signupForm!: FormGroup;
+  hidePassword = true;
+  passwordMinLength: number = 10;
+  
+  // To display the list of countries
+  countries: string[]= jsonFile.listOfCountries.map(country => country.name);
+  filteredCountries!: Observable<string[]>;
+
+  onSubmit(form: FormGroup): User {
+    this.newUser = SignupFormComponent.formGroupToUserObject(form);
+    console.log(this.newUser);
+    return this.newUser;
   }
 
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(this.passwordMinLength),
+        ],
+      }),
+      firstname: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      surname: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      street: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      country: new FormControl('', {
+        validators: [countryValidator(this.countries), Validators.required],
+      }),
+      postalCode: new FormControl('', {
+        validators: [Validators.required, postalCodeValidator()],
+      }),
+      city: new FormControl('', {
+        validators: [Validators.required],
+      }),
+    });
+
+    this.filteredCountries = this.signupForm.controls['country'].valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+  }
+  
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.countries.filter(country => country.toLowerCase().includes(filterValue));
+  }
+
+  private static formGroupToUserObject(form: FormGroup): User {
+    const value = form.value;
+    return {
+      username: value.username,
+      firstname: value.firstname,
+      surname: value.surname,
+      password: value.password,
+      address: {
+        street: value.street,
+        country: value.country,
+        city: value.city,
+        postalCode: value.postalCode 
+      }
+    }
+  }
+
+  private static fillForm(user: User, form: FormGroup): void {
+    form.setValue({
+      username: user.username,
+      firstname: user.firstname,
+      surname: user.surname,
+      password: user.password,
+      street: user.address.street,
+      country: user.address.country,
+      city: user.address.city,
+      postalCode: user.address.postalCode
+    });
+  }
+  
 }
--- frontend/src/app/signup-page/signup-page.component.ts
+++ frontend/src/app/signup-page/signup-page.component.ts
@@ -22,15 +22,15 @@ interface Response {
 })
 export class SignupPageComponent implements OnInit {
   constructor(private http: HttpClient, private backendLink: BackendLinkService) {
-    const usrStr = localStorage.getItem('newUser');
-    if (typeof usrStr === 'string') {
-      this.newUser = JSON.parse(usrStr);
-    } else {
-      this.newUser = new User();
-    }
+    // const usrStr = localStorage.getItem('newUser');
+    // if (typeof usrStr === 'string') {
+    //   this.newUser = JSON.parse(usrStr);
+    // } else {
+    //   this.newUser = new User();
+    // }
   }
 
-  private newUser: User;
+  private newUser!: User;
   
 
   //// Validators
@@ -57,9 +57,7 @@ export class SignupPageComponent implements OnInit {
   }
 
   displayArrow: boolean = true;
-  hidePassword = true;
 
-  passwordMinLength: number = 10;
 
   // display down arrow if the user has not scrolled to the bottom of the page
   @HostListener('window:scroll', ['$event'])
@@ -73,64 +71,16 @@ export class SignupPageComponent implements OnInit {
   }
 
 
-  // To display the list of countries
-  countries: string[]= jsonFile.listOfCountries.map(country => country.name);
-  filteredCountries!: Observable<string[]>;
+
+  ngOnInit(): void {}
+
   
-
-  signupForm!: FormGroup;
-
-
-  ngOnInit(): void {
-    this.signupForm = new FormGroup({
-      username: new FormControl('', {
-        validators: [Validators.required]
-      }),
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(this.passwordMinLength)]
-      }),
-      firstname: new FormControl('', {
-        validators: [Validators.required]
-      }),
-      surname: new FormControl('', {
-        validators: [Validators.required]
-      }),
-
-      street: new FormControl('', {
-        validators: [Validators.required]
-      }),
-      country : new FormControl('', {
-        validators: [this.countryValidator(this.countries), Validators.required] 
-      }),
-      postalCode: new FormControl('', {
-        validators: [Validators.required, this.postalCodeValidator()]
-      }),
-      city: new FormControl('', {
-        validators: [Validators.required]
-      })
-    });
-
-    
-    this.filteredCountries = this.signupForm.controls['country'].valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
-
-    SignupPageComponent.fillForm(this.newUser, this.signupForm)
-    
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.countries.filter(country => country.toLowerCase().includes(filterValue));
-  }
 
   onSubmit(form: FormGroup) {
     //this.newUser = this.formFieldsToObject(form);
     //console.log(this.newUser);
-    this.newUser = SignupPageComponent.formGroupToUserObject(form);
-    localStorage.setItem('newUser', JSON.stringify(this.newUser));
+    //this.newUser = SignupPageComponent.formGroupToUserObject(form);
+    //localStorage.setItem('newUser', JSON.stringify(this.newUser));
     return this.http
       .post<Response>(this.backendLink.getRegisterUrl(), this.newUser).subscribe({
         next: (data: Response) => {
@@ -142,35 +92,6 @@ export class SignupPageComponent implements OnInit {
         }
       });
   }
-
-  private static formGroupToUserObject(form: FormGroup): User {
-    const value = form.value;
-    return {
-      username: value.username,
-      firstname: value.firstname,
-      surname: value.surname,
-      password: value.password,
-      address: {
-        street: value.street,
-        country: value.country,
-        city: value.city,
-        postalCode: value.postalCode 
-      }
-    }
-  }
-
-  private static fillForm(user: User, form: FormGroup): void {
-    form.setValue({
-      username: user.username,
-      firstname: user.firstname,
-      surname: user.surname,
-      password: user.password,
-      street: user.address.street,
-      country: user.address.country,
-      city: user.address.city,
-      postalCode: user.address.postalCode
-    });
-  } 
 
 }
 
