0386d8358c544f9c738e7ef89802dff324693da6
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-09-02 14:17:17 +0200

    add add-sock-page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { AddSockPageComponent } from './add-sock-page/add-sock-page.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
   {path: 'signup', component: SignupPageComponent},
   {path: 'home', component: HomePageComponent},
+  {path: 'add-sock', component: AddSockPageComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   {path: '**', component: PageNotFoundComponent}
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,10 +18,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 
 import {RouterModule} from '@angular/router';
+import { AddSockPageComponent } from './add-sock-page/add-sock-page.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +37,7 @@ import {RouterModule} from '@angular/router';
     PageNotFoundComponent,
     HomePageComponent,
     SignupPageComponent,
+    AddSockPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,6 +51,10 @@ import {RouterModule} from '@angular/router';
     MatIconModule,
     MatAutocompleteModule,
     ReactiveFormsModule,
+    MatSliderModule,
+    MatButtonToggleModule,
+    ColorPickerModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
