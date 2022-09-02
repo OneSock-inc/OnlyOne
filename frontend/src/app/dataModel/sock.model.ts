3c597e2eafc124ee4346364ddd12453952a435c3
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-09-02 18:07:51 +0200

    basic impl of post request

--- frontend/src/app/dataModel/sock.model.ts
+++ frontend/src/app/dataModel/sock.model.ts
@@ -1,7 +1,17 @@
-export interface Sock {
-    id: string
-    shoeSize: number
-    description: string
-    color: string
-    picture: string
+
+
+export enum SockType {
+    low,
+    high,
+    none
+}
+
+export class Sock {
+    id: string = '';
+    shoeSize: number = 0;
+    description: string = ''; 
+    color: string = '';
+    picture: string = '';
+    type: SockType = SockType.none;
+    owner: string='';
 }
