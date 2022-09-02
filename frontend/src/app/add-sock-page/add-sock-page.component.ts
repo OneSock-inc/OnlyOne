875f32ff0b9bce25262b4e0ecdf7c4ae6e1dba27
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-09-02 15:27:07 +0200

    remove code related to form

--- frontend/src/app/add-sock-page/add-sock-page.component.ts
+++ frontend/src/app/add-sock-page/add-sock-page.component.ts
@@ -13,53 +13,15 @@ export class AddSockPageComponent implements OnInit {
 
   displayArrow: boolean = true;
 
-  addSockForm!: FormGroup;
-
-  pictureButtonLabel = "Take picture";
-
-  sizeValue: number = 36;
-
-  pictureB64: DataUrl = "";
-  
-  maxPxBorder: number = 500;
-
-  sockColor: string = "#ffffff";
-  colorPickerLabel: string = "Choose color";
-
-  screenWidth!: string;
-  textColor: string = "#ffffff";
   
 
 
-  constructor(private imageCompress: NgxImageCompressService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.addSockForm = new FormGroup({
-      description: new FormControl('',{
-        validators: [Validators.required]
-      }),
-      picture: new FormControl('',{
-        validators: [Validators.required]
-      }),
-      shoeSize: new FormControl('',{
-        validators: [Validators.required]
-      }),
-      sockType: new FormControl('',{
-        validators: [Validators.required]
-      }),
-    });
-    this.screenWidth = this.getScreenWidth().toString();
   }
 
-  onSubmit(form: any): void {
-    alert("Sock added successfully");
-    // send to api
-    //pictureB64
-    //sockColor
-    //form.description
-    //form.password
-
-  }
+  
 
   // display down arrow if the user has not scrolled to the bottom of the page
   @HostListener('window:scroll', ['$event'])
@@ -70,46 +32,6 @@ export class AddSockPageComponent implements OnInit {
     else {
       this.displayArrow = true;
     }
-  }
-
-  selectFile(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      let reader = new FileReader();
-      reader.onload = (event: any) => {
-        this.compressFile(event.target.result)
-      }
-      reader.readAsDataURL(event.target.files[0]);
-      this.pictureButtonLabel = "Change picture";
-    }
-    else {
-      this.pictureButtonLabel = "Take picture";
-    }
-    
-  }
-  
-  compressFile(image: DataUrl) {   
-    this.imageCompress.compressFile(image, 1, 100, 100, this.maxPxBorder, this.maxPxBorder).then(
-      result => {
-        this.pictureB64 = result;
-      }
-    );
-  }
-
-  getScreenWidth(): number {
-    if (window.innerWidth < 500) {
-      let formWidth = document.getElementsByTagName("form")[0].clientWidth;
-      return Math.floor(formWidth * 0.8); // TODO : find a way to get the width of the form
-    }
-    return 500;
-  }
-
-  onColorChange(newColor: string): void {
-    this.colorPickerLabel = "Change color";
-    let colorShower = document.getElementById("colorShower");
-    if (colorShower) {
-      colorShower.style.backgroundColor = newColor;
-    }
-    
   }
 
 }
