4b217d2dd5e520fcce1e59f0d8a5ce5bc1b795b9
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-09-03 23:22:02 +0200

    fix test config

--- frontend/src/app/services/socks-manager.service.spec.ts
+++ frontend/src/app/services/socks-manager.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { services } from '.';
 
 import { SocksManagerService } from './socks-manager.service';
 
@@ -6,7 +9,10 @@ describe('SocksManagerService', () => {
   let service: SocksManagerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, HttpClientModule],
+      providers: [services, HttpClient],
+    });
     service = TestBed.inject(SocksManagerService);
   });
 
