e198e8fccdc822f0f2d488c21e94e993fd897541
jorisSchaller <joris.schaller@heig-vd.ch>
2022-09-04 17:06:09 +0200

    Use signup form for my-account

--- frontend/src/app/my-account/my-account.component.ts
+++ frontend/src/app/my-account/my-account.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+  styleUrls: ['./my-account.component.scss'],
+  host: {'class': 'default-layout'}
 })
 export class MyAccountComponent implements OnInit {
 
