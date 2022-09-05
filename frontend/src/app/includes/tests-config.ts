0a6eb358400774939ad96e6ffffd71089c2c010a
superjeffcplusplus <72446750+superjeffcplusplus@users.noreply.github.com>
2022-09-05 13:48:42 +0200

    Frontend: more consistent scss  (#83)
    * refactor variables usage
    
    * remove mat-typography body class
    
    * change for use of global css for forms
    
    * fix default layout not alway applying
    
    * rename files
    
    * add import general
    
    * fix css var name
    
    * use default global class for form styling
    
    * use default class for form styling
    
    * new font weight and size for h1
    
    * add margin-0  class
    
    * add padding in default layout
    
    * ref use of global css class
    
    * fix margins for default layout

---
+++ frontend/src/app/includes/tests-config.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { SignupFormComponent } from "../forms/signup-form/signup-form.component";
+import { LoaderComponent } from "../loader/loader.component";
+import { LoaderDirective } from "../loader/loader.directive";
+import { LoginPageComponent } from "../login-page/login-page.component";
+import { MesageBannerDirective } from "../message-banner/mesage-banner.directive";
+import { MessageBannerComponent } from "../message-banner/message-banner.component";
+import { services } from "../services";
+
+export const testConfig = {
+    imports: [HttpClientTestingModule, HttpClientModule, ReactiveFormsModule, MatAutocompleteModule],
+    declarations: [
+      SignupFormComponent,
+      LoginPageComponent,
+      MesageBannerDirective,
+      MessageBannerComponent,
+      LoaderComponent,
+      LoaderDirective,
+    ],
+    providers: [services]
+  }
