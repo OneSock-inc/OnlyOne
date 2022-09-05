5511c819c20cf33761d41177715646abfbe0b429
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-09-05 08:57:37 +0200

    add matching-socks page

--- frontend/src/app/proposed-sock/proposed-sock.component.ts
+++ frontend/src/app/proposed-sock/proposed-sock.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Sock } from '../dataModel/sock.model';
 
 @Component({
   selector: 'app-proposed-sock',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProposedSockComponent implements OnInit {
 
+  @Input() // to be accessed by the parent component
+  sock : Sock = {
+    id: "",
+    description: "",
+    shoeSize: 0,
+    color: "",
+    picture:""
+  };
+
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  accept(sockId : string){
+    console.log("accepting sock " + sockId);
+  }
+
+  refuse(sockId : string){
+    console.log("refusing sock " + sockId);
+  }
 }
