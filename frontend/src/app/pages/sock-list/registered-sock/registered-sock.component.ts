53871ce97c221abb4f043848f73b564bd276ddfa
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-09-07 19:41:50 +0200

    create Match Service

--- frontend/src/app/pages/sock-list/registered-sock/registered-sock.component.ts
+++ frontend/src/app/pages/sock-list/registered-sock/registered-sock.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
+import { MatchService } from 'src/app/services/match/match-service.service';
 import {
   SocksManagerService,
   UserSocks,
@@ -16,7 +17,8 @@ import { Sock, typeToString as tts } from '../../../dataModel/sock.model';
 export class RegisteredSockComponent implements OnInit {
   constructor(
     private socksManager: SocksManagerService,
-    private router: Router
+    private router: Router,
+    private matchSrv: MatchService
   ) {}
 
   @Input() // to be accessed by the parent component
@@ -43,6 +45,8 @@ export class RegisteredSockComponent implements OnInit {
           `match-${url}`,
           { queryParams: { mySock: this.sock.id, otherSock: this.sock.match } },
         ];
+        this.matchSrv.selfSock = this.sock;
+        this.matchSrv.otherSockId = this.sock.match; 
       } else {
         this.badgeColor = 'red';
         this.socksManager
---
+++ frontend/src/app/services/match/match-service.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, mergeMap, Observable } from 'rxjs';
+import { Sock } from 'src/app/dataModel/sock.model';
+import { User } from 'src/app/dataModel/user.model';
+import { BackendLinkService } from '../backendservice/backend-link.service';
+import { SocksManagerService } from '../socksManager/socks-manager.service';
+
+
+export interface OtherInfo {
+  sock: Sock;
+  user: User;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MatchService {
+  constructor(
+    private sockManager: SocksManagerService,
+    private httpClient: HttpClient,
+    private bl: BackendLinkService
+  ) {}
+
+  otherSockId: string = '';
+  selfSockId: string = '';
+
+  otherSock?: Sock;
+  selfSock?: Sock;
+
+  getSelfSock(): Observable<Sock> {
+    return new Observable<Sock>((s) => s.next(this.selfSock));
+  }
+
+  getOtherInfos(): Observable<OtherInfo> {
+    return this.sockManager.getSockById(this.otherSockId).pipe(
+      mergeMap((otherSock: Sock) => {
+        const url: string = `${this.bl.getUserUrl()}/${this.otherSock?.owner}`;
+        return this.httpClient.get<User>(url).pipe(
+          map((user: User) => {
+            return { user: user, sock: otherSock };
+          })
+        );
+      })
+    );
+  }
+  
+}
