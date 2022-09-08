822dc5bc0c5fe219290ccc3caa67a997800b7e3f
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-09-08 09:27:01 +0200

    fix test bugs

--- frontend/src/app/match-result/match-result.component.spec.ts
+++ frontend/src/app/match-result/match-result.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { services } from '../services';
+import { MatchService } from '../services/match/match-service.service';
 
 import { MatchResultComponent } from './match-result.component';
 
@@ -8,7 +11,9 @@ describe('MatchResultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MatchResultComponent ]
+      declarations: [ MatchResultComponent ],
+      providers: [ services, MatchService ],
+      imports: [ HttpClientModule ]
     })
     .compileComponents();
 
